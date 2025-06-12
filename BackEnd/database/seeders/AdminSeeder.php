bb04a9debbf2520c4f5be059e840f6eeb30698ed
cmartinf <cm_farina@hotmail.com>
2025-06-12 02:56:50 +0000

    Actualizo AdminSeeders

--- BackEnd/database/seeders/AdminSeeder.php
+++ BackEnd/database/seeders/AdminSeeder.php
@@ -3,21 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@ejemplo.com', // Reemplaza con el correo electrónico de administrador que desees
-            'password' => Hash::make('Boca_1981'), // Reemplaza la contraseña con la que desees
-        ]);
+        // Para evitar duplicados, mejor usar updateOrCreate
+        User::updateOrCreate(
+            ['email' => 'programacion@vanguardia'],
+            [
+                'name' => 'Programación',
+                'password' => Hash::make('grupo5'),
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
